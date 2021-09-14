b8aa29641cf7b250bda2c7c6f784fcbb71a3c1cc
Jaslan <Jaslan.dlnv@gmail.com>
2021-09-14 21:35:18 +0500

    Backend: added mongoose

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,11 @@
-import { TrackModule } from './track/track.module';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TrackModule } from './track/track.module';
 
 @Module({
-  imports: [TrackModule]
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.qiyqx.mongodb.net/music-platform(spotify)?retryWrites=true&w=majority'),
+    TrackModule
+  ],
 })
 export class AppModule {}
