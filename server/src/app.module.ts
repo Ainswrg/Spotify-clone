3adec360798e484a0b4e98916dd561b30b7ec8ac
Jaslan <Jaslan.dlnv@gmail.com>
2021-09-15 22:47:45 +0500

    Backend: setting-up adding, listens tracks

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { FileModule } from './file/file.module';
 import { TrackModule } from './track/track.module';
+import * as path from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.qiyqx.mongodb.net/music-platform(spotify)?retryWrites=true&w=majority'),
-    TrackModule
+    ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:admin@cluster0.qiyqx.mongodb.net/music-platform(spotify)?retryWrites=true&w=majority',
+    ),
+    TrackModule,
+    FileModule,
   ],
 })
 export class AppModule {}
--- server/src/track/track.controller.ts
+++ server/src/track/track.controller.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { ObjectId } from 'mongoose';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { CreateTrackDto } from './dto/create-track.dto';
@@ -10,8 +11,13 @@ export class TrackController {
   constructor(private trackService: TrackService) {}
 
   @Post()
-  create(@Body() dto: CreateTrackDto) {
-    return this.trackService.create(dto);
+  @UseInterceptors(FileFieldsInterceptor([
+    { name: 'picture', maxCount: 1 },
+    { name: 'audio', maxCount: 1 },
+  ]))
+  create(@UploadedFiles() files, @Body() dto: CreateTrackDto) {
+    const {picture, audio} = files
+    return this.trackService.create(dto, picture[0], audio[0]);
   }
 
   @Get()
@@ -28,9 +34,14 @@ export class TrackController {
   delete(@Param('id') id: ObjectId) {
     return this.trackService.delete(id);
   }
-  
+
   @Post('/comment')
   addComment(@Body() dto: CreateCommentDto) {
     return this.trackService.addComment(dto);
+  }
+
+  @Post('/listen/:id')
+  listen(@Param('id') id: ObjectId) {
+    return this.trackService.listen(id);
   }
 }
