23919868fb3e7af4f7430c25a43ed61b9e75153c
Jaslan <Jaslan.dlnv@gmail.com>
2021-09-16 21:15:50 +0500

    feat: added Navbar

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,28 @@
 import { Button } from '@mui/material';
 import React from 'react'
+import Navbar from '../components/Navbar';
 
 const Index = () => {
   return (
-    <div>
-      main page
-      <Button>Create</Button>
+    <>
+    <Navbar />
+    <div className="center">
+      <h1>Добро пожаловать</h1>
+      <h3>Здесь собраны лучшие треки</h3>
     </div>
+    
+    <style jsx>
+      {`
+        .center {
+          margin-top: 150px;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+        }
+      `}
+    </style>
+    </>
   )
 }
 export default Index;
