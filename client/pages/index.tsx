49d94e84e55955b139ee9e17b367b663b5b6a821
Jaslan <Jaslan.dlnv@gmail.com>
2021-09-16 16:00:50 +0500

    create next-client, added dependencies

---
+++ client/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material';
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div>
+      main page
+      <Button>Create</Button>
+    </div>
+  )
+}
+export default Index;
